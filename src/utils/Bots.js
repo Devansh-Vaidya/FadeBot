979c59a861cc40a0869ec60b7756e1a10ce8f094
Devansh Vaidya <99159017+Devansh-Vaidya@users.noreply.github.com>
2024-08-19 12:55:31 -0400

    Changed the font logo and styling, changed Bots.js to use dictionary and use the model correctly

--- src/utils/Bots.js
+++ src/utils/Bots.js
@@ -1,34 +1,24 @@
 import Groq from "groq-sdk";
 import configData from "../../config.json";
 
-const GroqBot = async (botName, promptMessages) => {
+// Global dictionary for bot models
+const botModels = {
+  "Llama 3": "llama-3.1-70b-versatile",
+  "Gemma 2": "gemma2-9b-it",
+};
+
+export default async function GroqBot(botName, promptMessages) {
   const apiKey = configData["GROQ_API_KEY"];
   const groq = new Groq({ apiKey: apiKey, dangerouslyAllowBrowser: true });
 
-  let model = "";
-
-  switch (botName) {
-    case "Llama 3":
-      model = "llama-3.1-70b-versatile";
-      break;
-    case "Gemma 2":
-      model = "gemma2-9b-it";
-      break;
-    default:
-      model = "llama-3.1-70b-versatile";
-      break;
-  }
-
-  console.log("Model: ", model);
+  console.log("Bot Model: ", botModels[botName]);
 
   let reponse = await groq.chat.completions.create({
     messages: promptMessages,
-    model: model,
+    model: botModels[botName],
   });
 
   let errorMessage = "The bot is not responding. Please try again later.";
 
   return reponse["choices"][0]["message"]["content"] || errorMessage;
-};
-
-export default GroqBot;
+}
