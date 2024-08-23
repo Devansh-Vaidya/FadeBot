7e10cb22b6e6fe58d5ba324999cc52130f2f60b2
Devansh Vaidya <99159017+Devansh-Vaidya@users.noreply.github.com>
2024-08-22 22:57:12 -0400

    Changed background

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,27 @@ export default function App() {
   const botModels = {
     "Llama 3": "llama-3.1-70b-versatile",
     "Gemma 2": "gemma2-9b-it",
-    "Mixtral 8x7B": "mixtral-8x7b-32768"
+    "Mixtral 8x7B": "mixtral-8x7b-32768",
   };
   const [model, setModel] = useState(Object.keys(botModels)[0]);
 
   // Change the model
   const changeModel = (modelName) => {
-    console.log(modelName, botModels[model])
+    console.log(modelName, botModels[model]);
     setModel(modelName);
   };
 
   return (
     <div className="min-h-screen">
       <div
-        className="absolute inset-0 bg-cover bg-center opacity-30"
+        className="absolute inset-0 bg-cover bg-center opacity-40"
         style={{ backgroundImage: `url(${bgWallpaper})` }}
       ></div>
-      <NavBarComp model={model} changeModel={changeModel} botModels={botModels} />
+      <NavBarComp
+        model={model}
+        changeModel={changeModel}
+        botModels={botModels}
+      />
       <Body model={botModels[model]} />
     </div>
   );
